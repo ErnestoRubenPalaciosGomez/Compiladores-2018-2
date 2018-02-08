f102fa3f740a154ef5ae1ad03803fbf5ba0b2594
ErnestoRubenPalaciosGomez <ernesto_palacios@ciencias.unam.mx>
2018-02-08 17:30:18 -0600

    lab
    lo que hicimos en el lab

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/HelloWorld.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Ernesto Palacios
+ */
+import java.io.*;
+
+public class HelloWorld {
+
+    public static void main(String args[]){
+        try {
+            Reader reader = new FileReader("src/main/resources/test.txt");
+            Yylex y = new Yylex(reader);
+            y.yylex();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(ex.getMessage() + " No se encontró el archivo;");
+        }catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+}
+
