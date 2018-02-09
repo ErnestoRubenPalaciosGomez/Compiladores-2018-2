de5e6890e2815fa965a0fcc87c525441041bea8c
ErnestoRubenPalaciosGomez <ernesto_palacios@ciencias.unam.mx>
2018-02-08 19:45:38 -0600

    actualizacion practica 1
    Practica 1 acomodada

--- Practicas/Practica1/src/main/java/com/mycompany/practica1/HelloWorld.java
+++ Practicas/Practica1/src/main/java/com/mycompany/practica1/HelloWorld.java
@@ -3,14 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.mavenproject1;
+package com.mycompany.practica1;
 
 /**
  *
  * @author Ernesto Palacios
  */
 import java.io.*;
-
+    
 public class HelloWorld {
 
     public static void main(String args[]){
